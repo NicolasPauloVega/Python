131efc70960f46e9ed0d3d954feacd403dcbca39
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-12 08:17:16 -0500

    A1

---
+++ python/fiesta.py
@@ -0,0 +1,4 @@
+print("-----------------Bienvenido a la noche de brujas-----------------")
+print("-----Ha llegado el momento de ser un heroe y salvar el mundo-----")
+print("-Nuestras fiesta esta llena de sorpresas divertidas y deliciosas-")
+
