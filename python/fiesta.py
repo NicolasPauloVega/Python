d5387c10976d3794c94bafa6abd1fe60f0a5f8bb
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-12 08:34:11 -0500

    A3

--- python/fiesta.py
+++ python/fiesta.py
@@ -4,3 +4,11 @@ print("-Nuestras fiesta esta llena de sorpresas divertidas y deliciosas-")
 
 print("Para poder dar ingreso se deben seguir las siguientes instrucciones: \n1.Debe ir disfrasado de alfun superheroe de marvel (Deedpool,Iron Man,Hulk,Capitan America)\n2.Debe ser mayor de edad (tener 18 años)\n3.No traer bebidas alcoholizdas de lugares externos(realizar requisa)\n4.No traer armas corto punzantes o blancas.")
 
+#Numero de persona que quieren ingresar
+count = 0
+
+#Numero de personas que no pueden ingresar por no cumplir requisitos.
+count1 = 0
+
+#Numero de personas que pueden ingresar ya que cumplen los requisitos.
+count2 = 0
