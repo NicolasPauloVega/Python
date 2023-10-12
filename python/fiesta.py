22cee9c7f037351ea4849851ee6438ce034e824d
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-12 08:28:36 -0500

    A2

--- python/fiesta.py
+++ python/fiesta.py
@@ -2,3 +2,5 @@ print("-----------------Bienvenido a la noche de brujas-----------------")
 print("-----Ha llegado el momento de ser un heroe y salvar el mundo-----")
 print("-Nuestras fiesta esta llena de sorpresas divertidas y deliciosas-")
 
+print("Para poder dar ingreso se deben seguir las siguientes instrucciones: \n1.Debe ir disfrasado de alfun superheroe de marvel (Deedpool,Iron Man,Hulk,Capitan America)\n2.Debe ser mayor de edad (tener 18 años)\n3.No traer bebidas alcoholizdas de lugares externos(realizar requisa)\n4.No traer armas corto punzantes o blancas.")
+
