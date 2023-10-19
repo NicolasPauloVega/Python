5db3aa319003eca77304eceec17854b8e3d1a74d
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-19 09:49:13 -0500

    A4

---
+++ python/Actividad Lista/estudiante.py
@@ -0,0 +1,13 @@
+aprendices = []
+edades = []
+
+for i in range(30):
+    aprendiz = input("Digita el nombre del aprendiz: ")
+    edad = int(input("Digita la edad del aprendiz: "))
+
+    aprendices.append(aprendiz)
+    edades.append(edad)
+
+    print(aprendices)
+    print(edades)
+
