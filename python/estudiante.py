70a03d01b13421f3fb4dbf9689ae3eaee8de39c4
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-20 17:07:54 -0500

    A5

--- python/estudiante.py
+++ python/estudiante.py
@@ -1,6 +1,8 @@
 aprendices = []
 edades = []
 
+instructor = "Estos son los aprendices de la Instructora Adriana Lucia Rincon Forero"
+
 for i in range(30):
     aprendiz = input("Digita el nombre del aprendiz: ")
     edad = int(input("Digita la edad del aprendiz: "))
@@ -8,5 +10,6 @@ for i in range(30):
     aprendices.append(aprendiz)
     edades.append(edad)
 
+    print(instructor)
     print(aprendices)
     print(edades)
