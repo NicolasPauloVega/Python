24b8a5f913238bb31dbc2b0a34fbb7b52c804e3d
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-20 18:55:25 -0500

    A5

---
+++ listas/insIns.py
@@ -0,0 +1,15 @@
+aprendices = []
+edades = []
+
+for i in range(2):
+    registro = input("Ingresa el nombre del aprendiz: ")
+    registro1 = int(input("Ingresa la edad del aprendiz: "))
+
+    aprendices.append(registro)
+    edades.append(registro1)
+
+    print(aprendices)
+    print(edades)
+
+    aprendices.insert(0, "Adriana Lucia")
+    edades.insert(0, "")
