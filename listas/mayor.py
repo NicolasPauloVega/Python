32cd0747d7cfc22bad5deb40c1c7737cd928069f
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-20 18:30:51 -0500

    A5

---
+++ listas/mayor.py
@@ -0,0 +1,20 @@
+aprendices = []
+edades = []
+
+for i in range(2):
+    registro = input("Ingresa el nombre del aprendiz: ")
+    registro1 = int(input("Ingresa la edad del aprendiz: "))
+
+    aprendices.append(registro)
+    edades.append(registro1)
+
+    print(aprendices)
+    print(edades)
+
+conteo = 0
+
+for registro1 in edades:
+    if registro1 == 18:
+        conteo+=1
+
+print(conteo)
