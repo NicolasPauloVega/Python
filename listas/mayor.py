393a01e2074add4713de7610d38fb638caf2ce4b
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-20 20:04:48 -0500

    A5

--- listas/mayor.py
+++ listas/mayor.py
@@ -1,7 +1,7 @@
 aprendices = []
 edades = []
 
-for i in range(2):
+for i in range(30):
     registro = input("Ingresa el nombre del aprendiz: ")
     registro1 = int(input("Ingresa la edad del aprendiz: "))
 
