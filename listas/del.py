6e81e2bde0c7d8ea073e5701154f136b9bbf55a8
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-20 20:03:32 -0500

    A5

---
+++ listas/del.py
@@ -0,0 +1,20 @@
+aprendices = ["Adriana Lucia"]
+edades = [0]
+
+for i in range(2):
+    registro = input("Ingresa el nombre del aprendiz: ")
+    registro1 = int(input("Ingresa la edad del aprendiz: "))
+
+    aprendices.append(registro)
+    edades.append(registro1)
+
+    print(aprendices)
+    print(edades)
+    continue
+
+eliminar = input('deseas eliminar a la instructora "Adriana Lucia" de la lista [Dijitar si o no]')
+
+if eliminar == "si":
+    aprendices.remove("Adriana Lucia")
+    edades.remove(0)
+print(f"Se elimino a la instructora adriana de la lista aprendices. \n{aprendices}")
