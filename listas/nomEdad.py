1e8e9c1e197ea8442e14ec35e85120f46caf1bec
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-22 07:33:19 -0500

    A5

--- listas/nomEdad.py
+++ listas/nomEdad.py
@@ -1,12 +1,14 @@
-aprendices = []
-edades = []
+aprendices = [] #Creamos una lista vacia llamada aprendices
+edades = []     #y otra llamada edades
 
-for i in range(30):
-    registro = input("Ingresa el nombre del aprendiz: ")
-    registro1 = int(input("Ingresa la edad del aprendiz: "))
+#Iniciamos un bucle for con una iteracion en un rango de 30.
+for i in range(2):
+    registro = input("Ingresa el nombre del aprendiz: ") #Creamos una variable la cual almacene el nombre de los aprendices
+    registro1 = int(input("Ingresa la edad del aprendiz: ")) #Creamos una variable la cual almacene la edad de los aprendices
 
-    aprendices.append(registro)
-    edades.append(registro1)
+    aprendices.append(registro) #Agregamos los valores de la variable registro en la lista de aprendices.
+    edades.append(registro1) #Agregamos los valores de la variable regitro1 en la lista de edades
 
+    #Creamos otro bucle for para unir las 2  listas y mostrar a los aprendices con su respectiva edad
     for registro, registro1 in zip(aprendices,edades):
-        print(registro,registro1)
+        print(f"El aprendiz {registro} Tiene {registro1} años de edad")
