5e1d619d7172a400c5711fb797aea2bfc917a685
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-22 08:50:54 -0500

    A5

--- listas/xApre.py
+++ listas/xApre.py
@@ -2,7 +2,7 @@ aprendices = [] #Creamos una lista vacia llamada aprendices
 edades = []     #y otra llamada edades
 
 #Iniciamos un bucle for con una iteracion en un rango de 30.
-for i in range(2):
+for i in range(30):
     registro = input("Ingresa el nombre del aprendiz: ") #Creamos una variable la cual almacene el nombre de los aprendices
     registro1 = int(input("Ingresa la edad del aprendiz: ")) #Creamos una variable la cual almacene la edad de los aprendices
 
