79f5e7465b26fa6a360094fe0ebf42c89cab7189
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-22 07:41:40 -0500

    A5

--- listas/Instruct.py
+++ listas/Instruct.py
@@ -1,15 +1,16 @@
-aprendices = []
-edades = []
+aprendices = [] #Creamos una lista vacia llamada aprendices
+edades = []     #y otra llamada edades
 
+#Iniciamos un bucle for con una iteracion en un rango de 30.
 for i in range(30):
-    registro = input("Ingresa el nombre del aprendiz: ")
-    registro1 = int(input("Ingresa la edad del aprendiz: "))
+    registro = input("Ingresa el nombre del aprendiz: ") #Creamos una variable la cual almacene el nombre de los aprendices
+    registro1 = int(input("Ingresa la edad del aprendiz: ")) #Creamos una variable la cual almacene la edad de los aprendices
 
-    aprendices.append(registro)
-    edades.append(registro1)
+    aprendices.append(registro) #Agregamos los valores de la variable registro en la lista de aprendices.
+    edades.append(registro1)    #Agregamos los valores de la variable regitro1 en la lista de edades
 
-aprendices.insert(0, "Adriana Lucia")
+aprendices.insert(0, "Adriana Lucia") #
 edades.insert(0, "")
 
-print(aprendices)
-print(edades)
+print(aprendices) #Imprimimos los resultados de las listas
+print(edades)     #y edades
--- listas/mayor.py
+++ listas/mayor.py
@@ -1,18 +1,20 @@
-aprendices = []
-edades = []
+aprendices = [] #Creamos una lista vacia llamada aprendices
+edades = []     #y otra llamada edades
 
+#Iniciamos un bucle for con una iteracion en un rango de 30.
 for i in range(30):
-    registro = input("Ingresa el nombre del aprendiz: ")
-    registro1 = int(input("Ingresa la edad del aprendiz: "))
+    registro = input("Ingresa el nombre del aprendiz: ")    #Creamos una variable la cual almacene el nombre de los aprendices
+    registro1 = int(input("Ingresa la edad del aprendiz: "))    #Creamos una variable la cual almacene la edad de los aprendices
 
-    aprendices.append(registro)
-    edades.append(registro1)
+    aprendices.append(registro) #Agregamos los valores de la variable registro en la lista de aprendices.
+    edades.append(registro1)    #Agregamos los valores de la variable regitro1 en la lista de edades.
 
-    print(aprendices)
-    print(edades)
+    print(aprendices) #Imprimimos los resultados de las listas
+    print(edades)     #y edades
 
-conteo = 0
+conteo = 0 #Creamos una variable la cual valla contando desde 0
 
+#Iniciamos un bucle que al momento de detectar que una persona tiene 18 años cuente de 1 en uno
 for registro1 in edades:
     if registro1 == 18:
         conteo+=1
