2c54008b6893c108a5df384a49676e85b6ad9e0a
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-26 06:55:39 -0500

    A5

---
+++ fails/fallos1.py
@@ -0,0 +1,15 @@
+#Añadir a la instructora a la lista
+
+aprendices = ["Juan Diego","Mathew Guarnizo","Maria Fernanda","Melissa","Nicolas Paulo","Miguel Angel","Stiven","Maicol Esteban","Kevin Londoño","Marlon","Maria Jose","Nataly","Camila","Stiwar","Lina Vanesa","Paula Garcia","Maria Paula","Vanesa","Kevin Hernandez","Yency","Matias","Sebastian Tovar","Cristian Peña","Dahiana","Kevin Eduardo","Santiago Tomas","Nicolas Fierro","Laura Benavidez","Willfran"]
+edades = []
+
+import ramdom
+
+for i in range(30):
+    edad = ramdom.radit(16,20)
+    edades.append(edad)
+    print(aprendices)
+    print(edades)
+    aprendices.insert(1, "Adriana Lucia")
+    print(aprendices)
+    print(edades)
---
+++ fails/fallos2.py
@@ -0,0 +1,35 @@
+#Indique un dato a buscar en la lista de aprendices
+
+aprendices = ["Juan Diego","Mathew Guarnizo","Maria Fernanda","Melissa","Nicolas Paulo","Miguel Angel","Stiven","Maicol Esteban","Kevin Londoño","Marlon","Maria Jose","Nataly","Camila","Stiwar","Lina Vanesa","Paula Garcia","Maria Paula","Vanesa","Kevin Hernandez","Yency","Matias","Sebastian Tovar","Cristian Peña","Dahiana","Kevin Eduardo","Santiago Tomas","Nicolas Fierro","Laura Benavidez","Willfran"]
+edades = []
+
+import random
+
+for i in range(30):
+    edad = random.randint(16,20)
+    edades.append(edad)
+
+print(aprendices)
+print(edades)
+
+entrar = input("Deseas buscar por nombre [N o n] o por edades[E o e]")
+
+if entrar == "e":
+    buscar = input("Digita el nombre del aprendiz que desea buscar: ")
+    
+    if buscar in aprendices:
+        encN = aprendices.index(buscar)
+        edad = edades[encN]
+        print(f"La persona que buscas se llama {buscar} y tiene {edad} años de edad")
+    else:
+        print("La persona a la que buscas no esta en las lista que posee el programa.")
+elif entrar == "n":
+    buscar = int(input("Digita la edad del aprendiz que desea buscar: "))
+    if buscar in edades:
+        encE = edades.index(buscar)
+        name = aprendices[encE]
+        print(f"El aprendiz que buscas tiene {buscar} años de edad y se llama {name}")
+    else:
+        print("La persona a la que buscas no esta en las lista que posee el programa.")
+else:
+    print("Error... La persona que buscas no esta en mis registros")
