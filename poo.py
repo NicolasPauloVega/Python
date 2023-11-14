4ab8e95689537642ff650ca0ded72f1ac859409e
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2023-11-14 10:32:45 -0500

    A8

--- poo.py
+++ poo.py
@@ -37,3 +37,89 @@ print("-------------------------------------------------------------------------
 
 #Crear una herencia
 
+class Coche():
+    """Una presentación sencilla de un coche."""
+    def __init__(self, marca, modelo, ano):
+        """Inicializamos los atributos para describir un coche."""
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.lecturaOdometro = 0 #<--- Con este medimos la velocidad (Km) del coche
+
+    def obtenerNombreDescriptivo(self):
+        """Regresa un nombre descriptivo."""
+        nombreLargo = str(self.ano) + ' ' + self.marca + ' ' + self.modelo
+        return nombreLargo.title() #<--- Pedimos a python que retorne los valores de nombreLargo con las iniciales en mayusculas.
+    
+    def leerOdometro(self):
+        """Muestra el kilometraje del cocehe."""
+        print("Este coche ha recorrido " + str(self.lecturaOdometro) + "Km")
+
+    def actualizarOdometro(self, kilometraje):
+        """
+        Establecer la lectrura del odometro al valor asignado.
+        Cancela los cambios en caso de retroceder el valor.
+        """
+        if kilometraje >= self.lecturaOdometro:
+            self.lecturaOdometro = kilometraje
+        else:
+            print("No puedes retroceder el valor del odómetro.")
+
+    def incrementarOdometro(self, kilometros):
+        """Adiciona la cantidad asignada a la lectura del odómetro."""
+        self.lecturaOdometro += kilometros
+
+    def llenarTanqueGas(self):
+        print("tanque lleno. ")
+
+class CocheElec(Coche):
+    """Representar las caracteristicas de un coche, especificamente coches electricos."""
+    def __init__(self, marca, modelo, ano):
+        """
+        Inicializa los atributos de la clase padre.
+        """
+        super().__init__(marca,modelo,ano) #<--- Super() llama a la super clase en este caso Coche
+        self.kwh = 70
+    
+    def descripcionBateria(self):
+        """Imprime una frase describiendo el tamaño de la bateria."""
+        print("Este coche tiene una bateria de " + str(self.kwh) + "kwh.")
+
+    def llenarTanqueGas(self):
+        """Los coches electricos no necesitan un tanque de gasolina."""
+        print("Este coche no necesita un tanque d gasolina.")
+
+miTesla = CocheElec('tesla', 'modelo s', 2022)
+print(miTesla.obtenerNombreDescriptivo())
+miTesla.descripcionBateria()
+miTesla.llenarTanqueGas()
+
+print("---------------------------------------------------------------------------------------------------------------------")
+
+
+#Polismorfismo en python
+
+class carro():
+    """Mostrar el desplazamiento de un carro."""
+    def desplazamiento(self):
+        print("Me desplazo utilizando 4 ruedas.")
+    
+class moto():
+    """Mostrar el desplazamiento de una moto."""
+    def desplazamiento(self):
+        print("Me desplazo utilizando 2 ruedas. ")
+
+class camion():
+    """Mostrar el desplazamiento de un camion."""
+    def desplazamiento(self):
+        print("Me desplazo utilizando 6 ruedas. ")
+
+"""Cambia de forma el vahiculo para encontrar la clase que necesitemos"""
+def desplazamientoVehiculo(vehiculo):
+    vehiculo.desplazamiento()
+
+miVehiculo = camion()
+
+desplazamientoVehiculo(miVehiculo)
+
+print("---------------------------------------------------------------------------------------------------------------------")
