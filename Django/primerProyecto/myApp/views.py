0d333980994370083f4def2f2365b2554982ca08
Vanesa7778 <vanesaladinogomez@gmail.com>
2024-02-15 07:47:18 -0500

    b2

--- Django/primerProyecto/myApp/views.py
+++ Django/primerProyecto/myApp/views.py
@@ -18,6 +18,9 @@ layout = """
             <li>
                 <a href="/">Inicio</a>
             </li>
+            <li>
+                <a href="/contacto">Contacto</a>
+            </li>
         </ul>
         """
 
