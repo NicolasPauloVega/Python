dd42ab133b337e7adf85faac7524088f162c4fb8
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-03-14 10:08:56 -0500

    b2

--- Django/primerProyecto/myApp/models.py
+++ Django/primerProyecto/myApp/models.py
@@ -4,23 +4,38 @@ from django.db import models
 # Definición del modelo Article (Artículo)
 class Article(models.Model):
     # Campo para el título del artículo, como "Cómo Aprender Python Fácilmente"
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, verbose_name="Titulo")
     # Campo para el contenido del artículo, donde escribirías el texto principal del artículo
-    content = models.TextField()
+    content = models.TextField(verbose_name="Contenido")
     # Campo para la imagen del artículo, aquí puedes subir una imagen que acompañe al artículo
-    image = models.ImageField(default='null')
+    image = models.ImageField(default='null', verbose_name="imagen")
     # Campo para indicar si el artículo está disponible públicamente o no (sí o no)
-    public = models.BooleanField()
+    public = models.BooleanField(verbose_name="¿Publicado?")
     # Campo para la fecha y hora en que se creó el artículo
-    create_date = models.DateTimeField(auto_now_add=True)
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
     # Campo para la fecha de la última vez que se actualizó el artículo
-    update_date = models.DateField(auto_now=True)
+    update_date = models.DateField(auto_now=True, verbose_name="Fecha actual")
+    #Creamos una clase Meta
+    class Meta:
+        #db_table=""
+        verbose_name="Articulo"
+        verbose_name_plural="Articulos"
+        #Ordenamos de forma ascendente los articulos
+        ordering=['id']
+    def __str__(self):
+        if self.public:
+            publico:
 
 # Definición del modelo Category (Categoría)
 class Category(models.Model):
     # Campo para el nombre de la categoría, como "Tecnología" o "Deportes"
-    name = models.CharField(max_length=110)
+    name = models.CharField(max_length=110, verbose_name="Nombre")
     # Campo para describir de qué se trata la categoría, por ejemplo, "Últimas novedades sobre tecnología"
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, verbose_name="Describcion")
     # Campo para la fecha en que se creó la categoría
-    create_date = models.DateField()
+    create_date = models.DateField(verbose_name="Fecha de creacion")
+    #creamos una clase Meta para cambiar la visualizacion (nombre) de categoria
+    class Meta:
+        #db_table=""
+        verbose_name="Categoria"
+        verbose_name_plural="Categorias"
